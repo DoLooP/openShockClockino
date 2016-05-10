a0a9f45d70054661d267074abad642a878947e25
DoLooP <doloop.fr@gmail.com>
2016-05-10 18:03:21 +0200

    test tca9548a multiplexer scanner code

--- src/LowLatencyLogger/sensorDetect.cpp
+++ src/LowLatencyLogger/sensorDetect.cpp
@@ -1,11 +1,15 @@
 #include "arduino.h"
 #include <Wire.h>
+extern "C" {
+#include "utility/twi.h"  // from Wire library, so we can do bus scanning
+}
 #include <SparkFun_MMA8452Q.h>
 #include "myAssert.h"
 
 MMA8452Q  accel1(0x1D), accel2(0x1C);	// i2c adress as constructor
 
 #define TCAADDR 0x70
+#define RTCADDR 0x68
 
 class Multiplexer
 {
@@ -29,39 +33,28 @@ enum SENSORS { NONE, MMA8452Q_0x1c, MMA8452Q_0x1d} sensors[8];
 
 #define DEBUG_SENSORDETECT
 
-void sensorDetect()
+void sensorDetect()	// this is adafruit code from https://learn.adafruit.com/adafruit-tca9548a-1-to-8-i2c-multiplexer-breakout/wiring-and-test
 {
-	int i = 8;
-	while (i--)
-	{
-#ifdef DEBUG_SENSORDETECT
-		Serial.print("Testing channel ");
-		Serial.print(i);
-		Serial.println(" ...");
-		Serial.println("MMA8452Q_0x1d ?");
-#endif
-		tca9548a.switchI2C(i);
-		if (accel1.init(SCALE_8G, ODR_400))
-		{
-			Serial.print("Found sensor MMA8452Q_0x1d connected on channel #");
-			Serial.println(i);
-			sensors[i] = MMA8452Q_0x1d;
-		}
+	while (!Serial);
+	delay(1000);
 
-#ifdef DEBUG_SENSORDETECT
-		Serial.println("MMA8452Q_0x1d ?");
-#endif
-		if (accel2.init(SCALE_8G, ODR_400))
-		{
-			Serial.print("Found sensor MMA8452Q_0x1c connected on channel #");
-			Serial.println(i);
-			sensors[i] = MMA8452Q_0x1c;
+	Wire.begin();
+
+	Serial.println("\nTCAScanner ready!");
+
+	for (uint8_t t = 0; t<8; t++) {
+		tca9548a.switchI2C(t);
+		Serial.print("TCA Port #"); Serial.println(t);
+
+		for (uint8_t addr = 0; addr <= 127; addr++) {
+			if (addr == TCAADDR) continue;
+			if (addr == RTCADDR) continue;
+
+			uint8_t data;
+			if (!twi_writeTo(addr, &data, 0, 1, 1)) {
+				Serial.print("Found I2C 0x");  Serial.println(addr, HEX);
+			}
 		}
 	}
-	/*
-		assert(accel1.init(SCALE_8G, ODR_400));
-		Serial.println("MMA8452Q #1 OK.");
-		assert(accel2.init(SCALE_8G, ODR_400));
-		Serial.println("MMA8452Q #2 OK.");
-	*/
+	Serial.println("\ndone");
 }
